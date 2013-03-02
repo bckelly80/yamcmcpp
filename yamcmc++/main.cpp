ace03aa4c7857c899d7aa193d360b4b40fc39cba
Brandon C. Kelly <bckelly80@gmail.com>
2013-03-02 09:34:25 -0800

    Initial Commit

---
+++ yamcmc++/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  yamcmc++
+//
+//  Created by Brandon Kelly on 3/2/13.
+//  Copyright (c) 2013 Brandon Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
