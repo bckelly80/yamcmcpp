b99702b38136bc463d557b6fa7c9a4ac20c916d1
Brandon C. Kelly <bckelly80@gmail.com>
2013-05-03 14:48:08 -0700

    Added yamcmc++.hpp file to conveniently include all the yamcmc++ header files in one line.

---
+++ yamcmc++/yamcmc++.hpp
@@ -0,0 +1,20 @@
+//
+//  yamcmc++.hpp
+//  yamcmc++
+//
+//  Created by Brandon C. Kelly on 5/3/13.
+//  Copyright (c) 2013 Brandon Kelly. All rights reserved.
+//
+
+#ifndef yamcmc___yamcmc___hpp
+#define yamcmc___yamcmc___hpp
+
+// Convenience header to load all the necessary header files to use yamcmc++.
+
+#include <yamcmc++/random.hpp>
+#include <yamcmc++/proposals.hpp>
+#include <yamcmc++/samplers.hpp>
+#include <yamcmc++/steps.hpp>
+#include <yamcmc++/parameters.hpp>
+
+#endif
