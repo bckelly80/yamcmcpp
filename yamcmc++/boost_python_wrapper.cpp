ec3728cf51bf6e0c309a5becfa36acf656134abb
Andy Becker <acbecker@gmail.com>
2013-05-30 18:45:54 -0700

    Added boost_python_wrapper to setup.py, and started on proposals.hpp

--- yamcmc++/boost_python_wrapper.cpp
+++ yamcmc++/boost_python_wrapper.cpp
@@ -1,7 +1,11 @@
 #include<boost/python.hpp>
 
 // include the headers for the wrapped functions and classes
+#include "parameters.hpp" 
+#include "proposals.hpp"
 #include "random.hpp"
+#include "samplers.hpp" 
+#include "steps.hpp"
 
 using namespace boost::python;
 
@@ -9,7 +13,25 @@ using namespace boost::python;
 BOOST_PYTHON_MODULE(Yamcmcpp){
     // declare the classes to wrap. remember to exposure all
     // constructors!
+
+    class_<NormalProposal>("NormalProposal",init<>())
+        .def(init<double>())
+        .def("Draw", &NormalProposal::Draw )
+        .def("LogDensity", &NormalProposal::LogDensity )
+        ;
+    
     class_<RandomGenerator>("RandomGenerator",init<>())
         .def("SetSeed", &RandomGenerator::SetSeed )
+        .def("SaveSeed", &RandomGenerator::SaveSeed )
+        .def("RecoverSeed", &RandomGenerator::RecoverSeed )
+        .def("exp", &RandomGenerator::exp )
+        .def("lognormal", &RandomGenerator::lognormal )
+        .def("powerlaw", &RandomGenerator::powerlaw )
+        .def("tdist", &RandomGenerator::tdist )
+        .def("chisqr", &RandomGenerator::chisqr )
+        .def("scaled_inverse_chisqr", &RandomGenerator::scaled_inverse_chisqr )
+        .def("gamma", &RandomGenerator::gamma )
+        .def("invgamma", &RandomGenerator::invgamma )
+        .def("uniform_integer", &RandomGenerator::uniform_integer )
         ;
 }
--- yamcmc++/setup.py
+++ yamcmc++/setup.py
@@ -12,14 +12,14 @@ extension_url     = "https://github.com/bckelly80/yamcmcpp"
 
 BOOST_DIR         = os.environ["BOOST_DIR"]
 ARMADILLO_DIR     = os.environ["ARMADILLO_DIR"]
-include_dirs      = [BOOST_DIR+"/include", ARMADILLO_DIR+"/include", numpy.distutils.misc_util.get_numpy_include_dirs()]
-library_dirs      = [BOOST_DIR+"/lib", ARMADILLO_DIR+"/lib"]
+include_dirs      = ["/usr/include/", BOOST_DIR+"/include", ARMADILLO_DIR+"/include", numpy.distutils.misc_util.get_numpy_include_dirs()]
+library_dirs      = ["/usr/lib64/", "/usr/lib/", BOOST_DIR+"/lib", ARMADILLO_DIR+"/lib"]
 
 # define the libraries to link with the boost python library
-libraries = [ "boost_python" ]
+libraries = [ "boost_python", "lapack", "blas"]
 
 # define the source files for the extension
-source_files = [ "random.cpp" ]
+source_files = [ "boost_python_wrapper.cpp", "random.cpp" ]
  
 # create the extension and add it to the python distribution
 setup( name=extension_name, 
@@ -27,7 +27,7 @@ setup( name=extension_name,
        author="Brandon Kelly and Andrew Becker",
        author_email="acbecker@gmail.com",
        packages=[extension_name],
-       package_dir = { '': 'src' },
+       package_dir = { "": "src" },
        url=extension_url,
        description="C++ version of Yet Another Markov Chain Monte Carlo sampler",
        long_description=desc,
